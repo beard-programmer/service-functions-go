358a47c763e45e96d38cc351a87c57d3bbf3e38b
victorshinkevich <supostatekx@gmail.com>
2023-11-01 04:03:18 +0300

    main function

---
+++ cmd/service-functions-go/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"service-functions-go/internal/payroll"
+)
+
+func main() {
+	fmt.Println("Hello main")
+
+	employeePayroll := payroll.NewEmployeePayroll(1, []payroll.LineItem{
+		payroll.NewLineItem(100, "salary"),
+		payroll.NewLineItem(150, "bonus"),
+		payroll.NewLineItem(200, "meal_voucher"),
+	})
+	result, err := payroll.CalculateEmployeePayroll(payroll.BuildLineItemWithPolicy, payroll.CalculateTaxes, employeePayroll)
+	if err != nil {
+		fmt.Printf("Failed to calculate, reason: %v\n", err)
+	} else {
+		fmt.Printf("Calculated ep: %v and got result: %v\n", employeePayroll, result)
+	}
+}
